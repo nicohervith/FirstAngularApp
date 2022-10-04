be56d9e0e096e57bb5fc047479891028265d7e53
nicohervith <niko_hervith@hotmail.com>
2022-10-04 16:03:22 -0300

    new components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Terminar primer módulo de Angular',
+    day: 'Agosto 5 a las 12:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Hacer las compras',
+    day: 'Agosto 5 a las 17:00',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'investigar sobre Bootstrap',
+    day: 'Agosto 6 a las 10:00',
+    reminder: false,
+  },
+  {
+    id: 4,
+    text: 'Leer un libro',
+    day: 'Agosto 5 a las 19:00',
+    reminder: true,
+  },
+];
